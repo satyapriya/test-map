e9b95f900dfa112467db39b8221837c730c212de
Satyapriya <subudhisatyapriya@gmail.com>
2015-12-22 17:46:38 +0530

    implement autocomplete-search in map page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,10 +28,99 @@ angular.module('starter.controllers', [])
 })
 
 .controller('mainCtrl', function ($scope, $log) {
-    $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-    $scope.options = {scrollwheel: false};
-    var events = {
-      places_changed: function (searchBox) {}
+  // $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
+  // $scope.options = {scrollwheel: false};
+  // var events = {
+  //   places_changed: function (searchBox) {}
+  // }
+  // $scope.searchbox = { template:'searchbox.tpl.html', events:events};
+ 
+ // map with auto complete
+
+  var marker;
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: {lat: 59.325, lng: 18.070}
+    });
+    var input = /** @type {!HTMLInputElement} */(
+        document.getElementById('pac-input'));
+
+    var types = document.getElementById('type-selector');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    var infowindow = new google.maps.InfoWindow();
+    
+    marker = new google.maps.Marker({
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: {lat: 59.327, lng: 18.067}
+    });
+    marker.addListener('click', toggleBounce);
+    
+    autocomplete.addListener('place_changed', function() {
+      infowindow.close();
+      marker.setVisible(true);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+      }
+      marker.setIcon(/** @type {google.maps.Icon} */({
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+      }));
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      infowindow.open(map, marker);
+    });
+
+    // Sets a listener on a radio button to change the filter type on Places Autocomplete.
+    function setupClickListener(id, types) {
+      var radioButton = document.getElementById(id);
+      radioButton.addEventListener('click', function() {
+        autocomplete.setTypes(types);
+      });
     }
-    $scope.searchbox = { template:'searchbox.tpl.html', events:events};
+
+    setupClickListener('changetype-all', []);
+    setupClickListener('changetype-address', ['address']);
+    setupClickListener('changetype-establishment', ['establishment']);
+    setupClickListener('changetype-geocode', ['geocode']);
+  }
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+  initMap();  
 });
